a87be02e3e229955e5be16f26e2b2f38bb97f323
23MinL <posing1606@gmail.com>
2025-07-03 02:25:49 +0900

    feat: ProductTowerService 의존성 주입 설정

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,6 +1,12 @@
-from sentence_transformers import SentenceTransformer
-from functools import lru_cache
+from app.services.user_tower_service import UserTowerService
+from app.services.product_tower_service import ProductTowerService
 
-@lru_cache()
-def get_embedding_model() -> SentenceTransformer:
-    return SentenceTransformer("jhgan/ko-sbert-multitask")
+
+def get_user_tower_service() -> UserTowerService:
+    """User Tower 서비스 의존성 주입"""
+    return UserTowerService()
+
+
+def get_product_tower_service() -> ProductTowerService:
+    """Product Tower 서비스 의존성 주입"""
+    return ProductTowerService()
