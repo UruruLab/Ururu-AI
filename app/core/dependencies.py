916d6505e2d21c8a6460e270ccaa38bcf523eb45
23MinL <posing1606@gmail.com>
2025-07-03 16:11:54 +0900

    feat: 레거시 호환성을 위한 get_embedding_model 함수 추가
    - SentenceTransformer 직접 반환하는 get_embedding_model 함수 추가
    - 기존 user.py에서 사용하던 import 에러 해결
    - EmbeddingService를 통한 모델 접근으로 일관성 유지

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+from sentence_transformers import SentenceTransformer
 from app.services.embedding_service import EmbeddingService
 from app.services.user_tower_service import UserTowerService
 from app.services.product_tower_service import ProductTowerService
@@ -7,10 +8,15 @@ from app.services.product_tower_service import ProductTowerService
 def get_embedding_service() -> EmbeddingService:
     return EmbeddingService()
 
+@lru_cache()
+def get_embedding_model() -> SentenceTransformer:
+    """임베딩 모델 의존성 주입 (레거시 호환성)"""
+    service = get_embedding_service()
+    return service.model
+
 def get_user_tower_service() -> UserTowerService:
     """User Tower 서비스 의존성 주입"""
     return UserTowerService(get_embedding_service())
-
 
 def get_product_tower_service() -> ProductTowerService:
     """Product Tower 서비스 의존성 주입"""
