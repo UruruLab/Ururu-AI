a797c1d9a0a9c4062c574c6feee6800e8feedd94
23MinL <posing1606@gmail.com>
2025-07-03 14:28:22 +0900

    feat: EmbeddingService 의존성 주입 시스템 구축
    - @lru_cache 데코레이터로 싱글톤 패턴 구현
    - UserTowerService와 ProductTowerService에 동일한 EmbeddingService 주입
    - Two-Tower 모델의 임베딩 공간 일관성 보장
    - 메모리 효율성과 성능 최적화

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,12 +1,17 @@
+from functools import lru_cache
+from app.services.embedding_service import EmbeddingService
 from app.services.user_tower_service import UserTowerService
 from app.services.product_tower_service import ProductTowerService
 
+@lru_cache()
+def get_embedding_service() -> EmbeddingService:
+    return EmbeddingService()
 
 def get_user_tower_service() -> UserTowerService:
     """User Tower 서비스 의존성 주입"""
-    return UserTowerService()
+    return UserTowerService(get_embedding_service())
 
 
 def get_product_tower_service() -> ProductTowerService:
     """Product Tower 서비스 의존성 주입"""
-    return ProductTowerService()
+    return ProductTowerService(get_embedding_service())
